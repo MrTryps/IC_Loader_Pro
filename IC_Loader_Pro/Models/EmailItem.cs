3925a2cd2230b9cf6c94133b402863907623b6ef
Michael Moore <Michael.Moore@dep.nj.gov>
2025-06-23 20:47:36 -0400

    working - need tyo add email logic

---
+++ IC_Loader_Pro/Models/EmailItem.cs
@@ -0,0 +1,64 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IC_Loader_Pro.Models
+{
+    public class EmailItem : PropertyChangedBase
+    {
+        private string _permanentId;
+        /// <summary>
+        /// The permanent, unchanging Internet Message ID of the email.
+        /// </summary>
+        public string PermanentId
+        {
+            get => _permanentId;
+            set => SetProperty(ref _permanentId, value);
+        }
+
+        private string _subject;
+        public string Subject
+        {
+            get => _subject;
+            set => SetProperty(ref _subject, value);
+        }
+
+        private DateTime _receivedTime;
+        public DateTime ReceivedTime
+        {
+            get => _receivedTime;
+            set => SetProperty(ref _receivedTime, value);
+        }
+
+        private string _senderName;
+        public string SenderName
+        {
+            get => _senderName;
+            set => SetProperty(ref _senderName, value);
+        }
+
+        private int _attachmentCount;
+        public int AttachmentCount
+        {
+            get => _attachmentCount;
+            set => SetProperty(ref _attachmentCount, value);
+        }
+
+        private string _PrefID;
+        public string PrefID
+        {
+            get => _PrefID;
+            set => SetProperty(ref _PrefID, value);
+        }
+
+        private string _DelID;
+        public string DelID
+        {
+            get => _DelID;
+            set => SetProperty(ref _DelID, value);
+        }
+    }
+}
---
+++ IC_Loader_Pro/Models/ShapeItem.cs
@@ -0,0 +1,48 @@
+﻿using ArcGIS.Core.Geometry; // Required for Polygon
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace IC_Loader_Pro.Models
+{
+    public class ShapeItem : PropertyChangedBase
+    {
+        private Polygon _geometry;
+        /// <summary>
+        /// The actual polygon geometry of the shape.
+        /// </summary>
+        public Polygon Geometry
+        {
+            get => _geometry;
+            set => SetProperty(ref _geometry, value);
+        }
+
+        private string _sourceFile;
+        /// <summary>
+        /// The name of the original file this shape came from (e.g., "attachment1.shp").
+        /// </summary>
+        public string SourceFile
+        {
+            get => _sourceFile;
+            set => SetProperty(ref _sourceFile, value);
+        }
+
+        private bool _isValid;
+        /// <summary>
+        /// A flag indicating if the shape passed our validation rules (is a polygon, has area, etc.).
+        /// </summary>
+        public bool IsValid
+        {
+            get => _isValid;
+            set => SetProperty(ref _isValid, value);
+        }
+
+        private string _validationMessage = "OK";
+        /// <summary>
+        /// A message explaining why a shape is not valid.
+        /// </summary>
+        public string ValidationMessage
+        {
+            get => _validationMessage;
+            set => SetProperty(ref _validationMessage, value);
+        }
+    }
+}
